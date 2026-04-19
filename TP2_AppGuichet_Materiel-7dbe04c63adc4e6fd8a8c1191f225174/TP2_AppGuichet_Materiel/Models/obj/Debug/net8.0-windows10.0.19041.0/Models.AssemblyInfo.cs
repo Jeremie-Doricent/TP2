128ef4ff344e05552a926884bd6cb3e8b1288b31
Jeremie-Doricent <6325048@cegepmontpetit.ca>
2026-04-18 20:21:50 -0400

    cscs

--- TP2_AppGuichet_Materiel-7dbe04c63adc4e6fd8a8c1191f225174/TP2_AppGuichet_Materiel/Models/obj/Debug/net8.0-windows10.0.19041.0/Models.AssemblyInfo.cs
+++ TP2_AppGuichet_Materiel-7dbe04c63adc4e6fd8a8c1191f225174/TP2_AppGuichet_Materiel/Models/obj/Debug/net8.0-windows10.0.19041.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00568bc0e57dca838e7ad72dd55e1d067091a09e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9a73b6c877ba175e206f95011307c98f2413b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
